7365d31bbfbd1b35528691f410c7e4d0ffce497c
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-04-11 00:10:48 +0600

    navbar done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="statistics">Statistics</Link>
-      <Link to="applied-jobs">Applied Jobs</Link>
-      <Link to="blog">Blog</Link>
+    <nav className="flex justify-between items-center py-6">
+      <div>
+        <Link to="/">
+          <span className="text-2xl font-bold text-black ">
+            Build Your Dream
+          </span>
+        </Link>
+      </div>
+      <div className=" flex gap-x-4	 text-xl text-slate-400 ">
+        <Link to="/">Home</Link>
+        <Link to="statistics">Statistics</Link>
+        <Link to="applied-jobs">Applied Jobs</Link>
+        <Link to="blog">Blog</Link>
+      </div>
+
+      <button className="primary-btn w-48 px-7 py-5	rounded-lg text-xl text-white">
+        Star Applying
+      </button>
     </nav>
   );
 };
